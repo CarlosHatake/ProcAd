9b8542e2fafa606561927ec997eed5f54175a9ce
CarlosHatake <carlos.hdz.lpez@gmail.com>
2024-03-12 09:28:09 -0600

    conexion

--- ProcAd/clsAccess.vb
+++ ProcAd/clsAccess.vb
@@ -12,11 +12,6 @@ Public Class clsAccess
         'usr = "sa"
         'pass = "12345#b"
 
-        'servidor = "148.223.153.43\MSSQLSERVER1"
-        servidor = "ATENEA\MSSQLSERVER1"
-        usr = "INT_SIS_App"
-        pass = "K3ccq#iup2b5"
-
 
         Select Case sistema
             Case "ProcAd"
